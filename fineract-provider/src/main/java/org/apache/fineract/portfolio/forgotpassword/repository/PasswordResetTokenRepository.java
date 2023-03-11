392f82d4939dd8ffe12fec9c15f7fbeb10be08cc
francisguchie <francisguchie@gmail.com>
2023-03-11 21:44:38 +0000

    license updates

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/forgotpassword/repository/PasswordResetTokenRepository.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/forgotpassword/repository/PasswordResetTokenRepository.java
@@ -17,18 +17,15 @@
  * under the License.
  */
 
-package org.apache.fineract.portfolio.dashboard.data;
+package org.apache.fineract.portfolio.forgotpassword.repository;
 
-import java.math.BigDecimal;
-@SuppressWarnings("unused")
-public class LoanOfficerPortfolioData {
-    private final BigDecimal amount;
-   
-    public LoanOfficerPortfolioData(final BigDecimal amount) {
-		this.amount = amount;
-    }
-	public BigDecimal getAmount() {
-		return amount;
-	}
-    
+import org.apache.fineract.portfolio.forgotpassword.domain.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+	PasswordResetToken findByToken(String token);
+
 }
